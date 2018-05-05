f51467144751d5415d6391b90f0a17363df5874b
Felipe Duarte <felipeduarte034@gmail.com>
2018-05-05 15:55:19 -0300

    commit + gitignore

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float sensibilidade = 10, h, v;
+    private float x, y;
+    public GameObject alvo;
+    public Transform linkPoint;
+    private Vector2 pontoInicio;
+
+    void Update()
+    {
+        //Movimentação W A S D
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        x = h * sensibilidade * Time.deltaTime;
+        y = v * sensibilidade * Time.deltaTime;
+
+        transform.Translate(x, y, 0);
+    }
+
+    void FixedUpdate()
+    {
+        pontoInicio.x = linkPoint.position.x;
+        pontoInicio.y = linkPoint.position.y;
+        RaycastHit2D hit = Physics2D.Raycast(pontoInicio, Vector2.up);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(pontoInicio, hit.point); //exibir a linha do raycast
+            if (hit.collider.tag == "Enemy")
+            {
+                alvo = hit.collider.gameObject;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Destroy(alvo);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviourPlus.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviourPlus : MonoBehaviour
+{
+    public float sensibilidade = 10, h, v;
+    private float x, y;
+    public GameObject alvo;
+    public Transform begin;
+    private Vector2 pontoInicio;
+    private Vector2 directionLook;
+    private Vector2 directionRay;
+    public Vector3 mousePosition;
+
+    void Update()
+    {
+        //Movimentação W A S D
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        x = h * sensibilidade * Time.deltaTime;
+        y = v * sensibilidade * Time.deltaTime;
+        transform.Translate(x, y, 0);
+
+        //Rotação com o mouse - olhar para o mouse
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        directionLook.x = mousePosition.x - transform.position.x;
+        directionLook.y = mousePosition.y - transform.position.y;
+        transform.up = directionLook;
+    }
+
+    void FixedUpdate()
+    {
+        pontoInicio.x = begin.position.x;
+        pontoInicio.y = begin.position.y;
+
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //Posição do mouse em relação a Camera
+        directionRay.x = mousePosition.x - transform.position.x;
+        directionRay.y = mousePosition.y - transform.position.y; //Direção, dada por um vetor (Geometria Analitica - Vetores)
+
+        RaycastHit2D hit = Physics2D.Raycast(pontoInicio, directionRay);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(pontoInicio, hit.point);
+            
+            if (hit.collider.tag == "Enemy")
+            {
+                alvo = hit.collider.gameObject;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Destroy(alvo);
+                }
+            }
+        }
+    }
+}
